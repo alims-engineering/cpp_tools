903b4fb5b353390485e1da58645c17087a5e7c24
name_lss01 <you@example.com>
2026-06-11 22:55:03 +0800

    123

--- include/cpp_tools/core/math/standard/backend/next_combination_unchecked.h
+++ include/cpp_tools/core/math/standard/backend/next_combination_unchecked.h
@@ -1,42 +1,72 @@
 #pragma once
 
-#include <iostream>
-
+#include <cpp_tools/core.hpp>
 
 namespace cpp_tools::core::math::standard::backend {
 
 	// ====================================
 	//		parameters
 	// ====================================
-	// combination_array : Combination state array.
+	// combination	: Combination state array.
 	// max_value	: Maximum allowed value in the combination. Start form 1 to max_value
-	template<typename Integer = int>
-	void next_combination_unchecked(Integer combination_array[], Integer max_value) {
-		combination_array[0]++;
+	//
+	// ====================================
+	//		remark
+	// ====================================
+	// It is generally used when size_of_combination (larger than, >) max_value.
+	// Because it will stop checking when the combination have all done
+	template<class Container, class Integer>
+	void next_combination_unchecked(
+		Container& combination,
+		Integer max_value
+	) {
+		namespace ct_container = cpp_tools::core::container::standard::backend;
 
-		for (int y = 0; y < max_value; y++) {
-			if (combination_array[y] > max_value - y) {
-				combination_array[y + 1]++;
+		ct_container::get(combination, 0)++;
 
-				for (int y2 = y; y2 >= 0; y2--) {
-					combination_array[y2] = combination_array[y2 + 1] + 1;
+		for (Integer y = 0; y < max_value; ++y) {
+			if (ct_container::get(combination, y) > max_value - y) {
+
+				ct_container::get(combination, y + 1)++;
+
+				for (Integer y2 = y; y2 >= 0; --y2) {
+					ct_container::get(combination, y2)
+						= ct_container::get(combination, y2 + 1) + 1;
 				}
 			}
 		}
 	}
 
+	// ====================================
+	//      parameters
+	// ====================================
+	// combination      : Current combination state.
+	// max_value        : Maximum allowed value in the combination.
+	//                    Valid values are in the range [1, max_value].
+	// combination_size : Number of active elements participating in the
+	//                    combination generation
+	// ====================================
+	//		remark
+	// ====================================
+	// It is generally used when size_of_combination (smaller than or equal to, <=) max_value.
 	template<class Container, class Integer>
-	void next_combination_unchecked(Container& combination, Integer max_value) {
-		backend::get(combination, 0)++;
+	void next_combination_unchecked(
+		Container& combination,
+		Integer max_value,
+		Integer combination_size
+	) {
+		namespace ct_container = cpp_tools::core::container::standard::backend;
 
-		for (Integer y = 0; y < max_value; ++y) {
-			if (backend::get(combination, y) > max_value - y) {
+		ct_container::get(combination, 0)++;
 
-				backend::get(combination, y + 1)++;
+		for (Integer y = 0; y < combination_size; ++y) {
+			if (ct_container::get(combination, y) > max_value - y) {
+
+				ct_container::get(combination, y + 1)++;
 
 				for (Integer y2 = y; y2 >= 0; --y2) {
-					backend::get(combination, y2)
-						= backend::get(combination, y2 + 1) + 1;
+					ct_container::get(combination, y2)
+						= ct_container::get(combination, y2 + 1) + 1;
 				}
 			}
 		}
